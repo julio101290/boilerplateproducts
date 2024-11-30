a131b2a35a90b545eeedfe2a63cfa52ab14dd071
Julio Cesar Leyva Rodriguez <juliocesarleyvarodriguez@hotmail.com>
2024-11-30 09:21:44 -0700

    Changes in the routes files

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -4,43 +4,84 @@
 $routes->group('admin', function ($routes) {
 
 
-    $routes->get('sucursales/usuariosPorSucursal/(:any)'
-    , 'BranchofficesController::usuariosPorSucursal/$1'
-    , ['namespace' => 'julio101290\boilerplatebranchoffice\Controllers']
-    );
 
-    $routes->resource('branchoffices', [
-        'filter' => 'permission:branchoffices-permission',
-        'controller' => 'branchofficesController',
-        'except' => 'show',
-        'namespace' => 'julio101290\boilerplatebranchoffice\Controllers',
+
+    $routes->resource('categorias', [
+        'filter' => 'permission:categorias-permission',
+        'controller' => 'categoriasController',
+        'except' => 'show', 
+        ['namespace' => 'julio101290\boilerplateproducts\Controllers']
     ]);
 
-    $routes->post('branchoffices/save'
-                , 'BranchofficesController::save'
-                , ['namespace' => 'julio101290\boilerplatebranchoffice\Controllers']
-            );
-    $routes->post('branchoffices/getBranchoffices'
-                , 'BranchofficesController::getBranchoffices'
-                , ['namespace' => 'julio101290\boilerplatebranchoffice\Controllers']
-                );
+    $routes->post('categorias/save'
+    , 'CategoriasController::save'
+    ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
+    );
+    
+    $routes->post('categorias/getCategorias'
+    , 'CategoriasController::getCategorias'
+    ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
+    );
+
+    $routes->post('categorias/buscarFolio'
+        , 'CategoriasController::getFolio'
+        ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
+        );
+
+    $routes->post('categorias/getCategoriasAjax'
+    , 'CategoriasController::getCategoriasAjax'
+    ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
+    );
 
 
-    $routes->post('usuariosSucursal/getUsuariosSucursal'
-                    , 'UsuariosSucursalController::getUsuariosSucursal'
-                    , ['namespace' => 'julio101290\boilerplatebranchoffice\Controllers']
-                );
+    $routes->resource('products', [
+        'filter' => 'permission:products-permission',
+        'controller' => 'productsController',
+        'except' => 'show'
+        ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
+    ]);
+    $routes->post('products/save'
+    , 'ProductsController::save'
+    ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
+    );
+
+    $routes->post('products/getProducts'
+    , 'ProductsController::getProducts'
+     ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
+    );
 
 
-    $routes->post('sucursales/activarDesactivar'
-                , 'BranchofficesController::activarDesactivar'
-                ,  ['namespace' => 'julio101290\boilerplatebranchoffice\Controllers']
-                );
-                
-    $routes->post('sucursales/getSucursalesAjax'
-                , 'BranchofficesController::getSucursalesAjax'
-                , ['namespace' => 'julio101290\boilerplatebranchoffice\Controllers']
-                );
+    $routes->get('products/getAllProducts/(:any)'
+    , 'ProductsController::getAllProducts/$1'
+    ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
+    );
+
+    $routes->get('products/barcode/(:any)'
+    , 'ProductsController::getBarcodePDF/$1'
+    ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
+    );
+
+    $routes->get('products/getAllProductsInventory/(:any)/(:any)/(:any)'
+    , 'ProductsController::getAllProductsInventory/$1/$2/$3'
+    ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
+    );
+
+    $routes->post('products/getUnidadSATAjax'
+            , 'ProductsController::getUnidadSATAjax'
+            ,['namespace' => 'julio101290\boilerplateproducts\Controllers']);
+
+
+    $routes->post('products/getProductosSATAjax'
+    , 'ProductsController::getProductosSATAjax'
+    ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
+    );
+
+    $routes->post('products/getProductsAjax'
+    , 'ProductsController::getProductsAjaxSelect2'
+    ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
+    );
+
+
 
 
 });
