b5f716084a03f1bbc12ddf7afe322c0688d2ea19
Julio Cesar Leyva Rodriguez <juliocesarleyvarodriguez@hotmail.com>
2024-11-30 09:12:30 -0700

    Changes in the migrations Files

--- src/Database/Migrations/2023-04-20051528_Categorias.php
+++ src/Database/Migrations/2023-04-20051528_Categorias.php
@@ -4,28 +4,25 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class UsuariosSucursal extends Migration
+class Categorias extends Migration
 {
     public function up()
     {
-        // Usuariosempresa
+        // Categorias
         $this->forge->addField([
             'id'                    => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
-            'idEmpresa'             => ['type' => 'int', 'constraint' => 11, 'null' => true],
-            'idSucursal'             => ['type' => 'int', 'constraint' => 11, 'null' => true],
-            'idUsuario'             => ['type' => 'int', 'constraint' => 11, 'null' => true],
-            'status'             => ['type' => 'varchar', 'constraint' => 8, 'null' => true],
-
+            'idEmpresa'             => ['type' => 'bigint', 'null' => true],
+            'clave'             => ['type' => 'varchar', 'constraint' => 8, 'null' => true],
+            'descripcion'             => ['type' => 'varchar', 'constraint' => 128, 'null' => true],
             'created_at'       => ['type' => 'datetime', 'null' => true],
             'updated_at'       => ['type' => 'datetime', 'null' => true],
             'deleted_at'       => ['type' => 'datetime', 'null' => true],
         ]);
-
         $this->forge->addKey('id', true);
-        $this->forge->createTable('usuarios_sucursal', true);
+        $this->forge->createTable('categorias', true);
     }
     public function down()
     {
-        $this->forge->dropTable('usuarios_sucursal', true);
+        $this->forge->dropTable('categorias', true);
     }
 }
