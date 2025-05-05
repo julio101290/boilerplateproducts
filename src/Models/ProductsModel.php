3b247744bc0ebc6c3333e0d950a5180404d3e7a5
juliocesar101290 <juliocesarleyvarodriguez@hotmail.com>
2025-05-05 12:29:45 -0700

    Changes in validation fields an portability with engine database  like MariaDB, ProgresSQL and SQLServer

--- src/Models/ProductsModel.php
+++ src/Models/ProductsModel.php
@@ -43,42 +43,52 @@ class ProductsModel extends Model {
     protected $useTimestamps = true;
     protected $createdField = 'created_at';
     protected $deletedField = 'deleted_at';
-    protected $validationRules = [];
+    protected $validationRules = [
+        
+        'barcode' => 'permit_empty|numeric',
+        'buyPrice' => 'numeric',
+        'salePrice' => 'numeric',
+        'stock' => 'numeric',
+        
+    ];
     protected $validationMessages = [];
     protected $skipValidation = false;
 
     public function mdlProductos($empresas) {
-        $resultado = $this->db->table('products a, empresas b')
-                ->select('a.id
-            ,a.code
-            ,a.idCategory
-            ,a.validateStock
-            ,a.inventarioRiguroso
-            ,a.description
-            ,a.stock
-            ,a.buyPrice
-            ,a.salePrice
-            ,a.porcentSale
-            ,a.porcentTax
-            ,a.routeImage
-            ,a.created_at
-            ,a.deleted_at
-            ,a.updated_at
-            ,a.barcode
-            ,a.unidad
-            ,b.nombre as nombreEmpresa
-            ,a.porcentIVARetenido
-            ,a.porcentISRRetenido
-            ,a.nombreUnidadSAT
-            ,a.nombreClaveProducto
-            ,a.unidadSAT
-            ,a.claveProductoSAT
-            ,a.inmuebleOcupado
-            ,a.tasaExcenta
-            ,a.predial
-            ')
-                ->where('a.idEmpresa', 'b.id', FALSE)
-                ->whereIn('idEmpresa', $empresas)
+
+
+        $resultado = $this->db->table('products a')
+                ->select(
+                        'a.id AS id,
+                        a.code AS code,
+                        a.idCategory AS idCategory,
+                        a.validateStock AS validateStock,
+                        a.inventarioRiguroso AS inventarioRiguroso,
+                        a.description AS description,
+                        a.stock AS stock,
+                        a.buyPrice AS buyPrice,
+                        a.salePrice AS salePrice,
+                        a.porcentSale AS porcentSale,
+                        a.porcentTax AS porcentTax,
+                        a.routeImage AS routeImage,
+                        a.created_at AS created_at,
+                        a.deleted_at AS deleted_at,
+                        a.updated_at AS updated_at,
+                        a.barcode AS barcode,
+                        a.unidad AS unidad,
+                        b.nombre AS nombreEmpresa,
+                        a.porcentIVARetenido AS porcentIVARetenido,
+                        a.porcentISRRetenido AS porcentISRRetenido,
+                        a.nombreUnidadSAT AS nombreUnidadSAT,
+                        a.nombreClaveProducto AS nombreClaveProducto,
+                        a.unidadSAT AS unidadSAT,
+                        a.claveProductoSAT AS claveProductoSAT,
+                        a.inmuebleOcupado AS inmuebleOcupado,
+                        a.tasaExcenta AS tasaExcenta,
+                        a.predial AS predial'
+                )
+                ->join('empresas b', 'a.idEmpresa = b.id')
+                ->whereIn('a.idEmpresa', $empresas)
                 ->where('a.deleted_at', null);
 
         return $resultado;
@@ -181,8 +191,6 @@ class ProductsModel extends Model {
         $this->db->query("DROP TABLE IF EXISTS tempProducts");
 
         $this->db->query("create table tempProducts " . $resultado->getCompiledSelect());
-        
-        
 
         return $this->db->table('tempProducts');
     }
@@ -279,41 +287,48 @@ class ProductsModel extends Model {
     }
 
     public function mdlGetProductoEmpresa($empresas, $idProducto) {
-        $resultado = $this->db->table('products a, empresas b, categorias c ')
-                        ->select('a.id
-            ,a.code
-            ,a.idEmpresa
-            ,a.validateStock
-            ,a.inventarioRiguroso
-            ,a.idCategory
-            ,c.clave
-            ,c.descripcion as descripcionCategoria
-            ,a.description
-            ,a.stock
-            ,a.buyPrice
-            ,a.salePrice,a.porcentSale
-            ,a.porcentTax,a.routeImage
-            ,a.created_at,a.deleted_at
-            ,a.updated_at
-            ,a.barcode
-            ,a.unidad
-            , b.nombre as nombreEmpresa
-            ,a.porcentIVARetenido
-            ,a.porcentISRRetenido
-            ,a.nombreUnidadSAT
-            ,a.nombreClaveProducto
-            ,a.unidadSAT
-            ,a.inmuebleOcupado
-            ,a.tasaExcenta
-            ,a.predial
-            ,a.claveProductoSAT')
-                        ->where('a.idEmpresa', 'b.id', FALSE)
-                        ->where('a.idCategory', 'c.id', FALSE)
-                        ->where('a.id', $idProducto)
-                        ->where('a.deleted_at', null)
-                        ->whereIn('a.idEmpresa', $empresas)->get()->getFirstRow();
+        
+        $resultado = $this->db->table('products a')
+                ->select(
+                        'a.id AS id,
+                        a.code AS code,
+                        a.idEmpresa AS idEmpresa,
+                        a.validateStock AS validateStock,
+                        a.inventarioRiguroso AS inventarioRiguroso,
+                        a.idCategory AS idCategory,
+                        c.clave AS clave,
+                        c.descripcion AS descripcionCategoria,
+                        a.description AS description,
+                        a.stock AS stock,
+                        a.buyPrice AS buyPrice,
+                        a.salePrice AS salePrice,
+                        a.porcentSale AS porcentSale,
+                        a.porcentTax AS porcentTax,
+                        a.routeImage AS routeImage,
+                        a.created_at AS created_at,
+                        a.deleted_at AS deleted_at,
+                        a.updated_at AS updated_at,
+                        a.barcode AS barcode,
+                        a.unidad AS unidad,
+                        b.nombre AS nombreEmpresa,
+                        a.porcentIVARetenido AS porcentIVARetenido,
+                        a.porcentISRRetenido AS porcentISRRetenido,
+                        a.nombreUnidadSAT AS nombreUnidadSAT,
+                        a.nombreClaveProducto AS nombreClaveProducto,
+                        a.unidadSAT AS unidadSAT,
+                        a.inmuebleOcupado AS inmuebleOcupado,
+                        a.tasaExcenta AS tasaExcenta,
+                        a.predial AS predial,
+                        a.claveProductoSAT AS claveProductoSAT'
+                )
+                ->join('empresas b', 'a.idEmpresa = b.id')
+                ->join('categorias c', 'a.idCategory = c.id')
+                ->where('a.id', $idProducto)
+                ->where('a.deleted_at', null)
+                ->whereIn('a.idEmpresa', $empresas)
+                ->get()
+                ->getFirstRow();
 
         return $resultado;
     }
-
 }
